96da65f7c7d810d8a609cb5d37daf7662724f8ba
Diego Echeverri <diegoeche@Multivac.(none)>
2009-03-31 00:21:35 +0200

    Adding support for tuples.

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ParallelListComp #-}
 import Analyser
 import qualified Data.Map as M hiding (split) 
 import Data.List
@@ -13,12 +14,12 @@ data Type = TInteger
           | Tuple [Type]
             deriving (Show,Eq)
 
+--tVariables = map return ['a'..'z'] ++ map (++ "'") tVariables
 
+tVariables = "a":map (++ "'") tVariables
 
-tVariables = map return ['a'..'z'] ++ map (++ "'") tVariables
-
-getEquations _ (Value (IPrim _)) t _ = return [(t,TInteger)]
-getEquations tEnv (Call s xs) t fresh = 
+getEquations _ [Value (IPrim _)] t _ = return [(t,TInteger)]
+getEquations tEnv [Call s xs] t fresh = 
     let (v:vs) = fresh
         -- We just make 2 different list for each recursive call. using the odd and even 
         -- Fresh vars
@@ -29,22 +30,47 @@ getEquations tEnv (Call s xs) t fresh =
                   Just x -> return [(t,x)]
                   Nothing -> fail $ show s ++ " type not found."
           [x] -> do
-                 first <- getEquations tEnv x vT (map fst vs')
-                 second <- getEquations tEnv (Call s []) (Arrow vT t) (map fst vs'')
+                 first <- getEquations tEnv [x] vT (map fst vs')
+                 second <- getEquations tEnv [Call s []] (Arrow vT t) (map fst vs'')
                  return $ first ++ second
-          xs -> --let () 
-              do
-                 first <- concat $ mapM (\par -> getEquations tEnv par vT (map fst vs')) xs
-                 second <- getEquations tEnv (Call s []) (Arrow vT t) (map fst vs'')
+          xs -> do
+                 first <- getEquations tEnv xs vT (map fst vs')
+                 second <- getEquations tEnv [Call s []] (Arrow vT t) (map fst vs'')
                  return $ first ++ second
+getEquations tEnv tuple t fresh = 
+    let (used,nonUsed) = splitAt (length tuple) fresh
+        -- ["a","a'"...] -> [["a","a'"...],["b","b'"...],...]
+        -- Please don't use to much tuples or we can run out of fresh vs. :P
+        freshVarsLists = (iterate . map) (\(x:xs) -> succ x:xs) nonUsed
+        --[(vars,ts,tuples)]
+        vars = map TVar used
+        params = zip3 freshVarsLists vars (map return tuple)
+    in do 
+      tupleEquations <- foldM folder [] params
+      return $ tupleEquations ++ [(t, Tuple vars)]
+          where folder acc (x,y,z) = do 
+                  eq <- getEquations tEnv z y x
+                  return $ acc ++ eq
 
 resolveEquations [] = fail "No equations"
 resolveEquations ((x, y):xs) | x == y = resolveEquations xs
 resolveEquations ((Arrow t1 t2, Arrow t3 t4):xs) = resolveEquations ((t1,t3):(t2,t4):xs)
-resolveEquations ((TVar x, t):xs) | xs /= [] = resolveEquations $ map (tmap $ replace x t) xs
-resolveEquations ((t, TVar x):xs) | xs /= [] = resolveEquations $ map (tmap $ replace x t) xs
+
+resolveEquations ((TVar x, t):xs) 
+    | xs /= [] && isType t = resolveEquations $ map (tmap $ replace x t) xs
+resolveEquations ((t, TVar x):xs) 
+    | xs /= [] && isType t = resolveEquations $ map (tmap $ replace x t) xs
+
+resolveEquations ((Tuple tx, Tuple ty):xs) 
+    | length tx == length ty = resolveEquations $ zip tx ty ++ xs
+    | otherwise = fail $ "The arity of " ++ show tx ++ " and " ++ show ty ++ "Doesn't match"
+
 resolveEquations [x] = return x
-resolveEquations x =  fail "Not possible to unify"
+resolveEquations x =  fail $ "Not possible to unify: " ++ show x
+
+isType (TVar _) = False
+isType (Tuple x) = all isType x
+isType _ = True
 
 replace x y (Arrow t1 t2) = Arrow (replace x y t1) (replace x y t2)
 replace x y (Tuple l) = Tuple $ map (replace x y) l
@@ -53,9 +79,17 @@ replace x y z  = z
 
 tmap f (a,b) = (f a, f b)
 
---getEquations (M.fromList [("id", Arrow (TVar "A") (TVar "A"))]) (Call "id" []) (TVar "T") tVariables >>= resolveEquations 
---getEquations (M.fromList [("id", Arrow (TVar "A") (TVar "A"))]) (Call "id" [int 4]) (TVar "T") tVariables >>= resolveEquations 
+--getEquations (M.fromList [("id", Arrow (TVar "A") (TVar "A"))]) [Call "id" []] (TVar "T") tVariables >>= resolveEquations 
+--getEquations (M.fromList [("id", Arrow (TVar "A") (TVar "A"))]) [Call "id" [int 4]] (TVar "T") tVariables >>= resolveEquations 
 
-typeEnv = (M.fromList [("id", Arrow (TVar "A") (TVar "A")),("+", Arrow (Tuple [TInteger,TInteger]) TInteger)])
-int n = Value $ IPrim n
-eq = getEquations typeEnv (Call "+" [int 3, int 5]) (TVar "T") tVariables
+typeEnv = M.fromList [("id", Arrow (TVar "A") (TVar "A")),
+                      ("+", Arrow (Tuple [TInteger,TInteger]) TInteger),
+                      ("==", Arrow (Tuple [TInteger,TInteger]) TBoolean),
+                      ("ifThenElse", Arrow (Tuple [TBoolean, TVar "IF", TVar "IF"]) $ TVar "IF")]
+int = Value . IPrim
+--eq = getEquations typeEnv [Call "+" [int 3, int 5]] (TVar "T") tVariables >>= resolveEquations
+
+a () = getEquations typeEnv [Call "ifThenElse" 
+                               [Call "==" [int 3, int 3], 
+                                     int 5, 
+                                         int 5]] (TVar "T") tVariables >>= resolveEquations
