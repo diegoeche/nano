4e3a2b4fecb57a47b11ea53e6184451eb3ad41c1
Diego Echeverri <diegoeche@Multivac.(none)>
2009-08-14 15:58:27 -0500

    Changes

--- Main.hs
+++
@@ -1,7 +0,0 @@
-import Interpreter
-import Control.Monad.State
-import qualified Environment as E
-
-main :: IO ((), Interpreter.Environment)
-main = 
-    runStateT loop (Env E.types E.definitions E.operators)
